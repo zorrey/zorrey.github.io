ba474eecc1e3a2af453b43ec6bd48c2b1298ec4b
Zorrey <102656748+zorrey@users.noreply.github.com>
2023-04-01 17:01:21 +1100

    new project named message-board added

---
+++ view/project_list.js
@@ -0,0 +1,14 @@
+
+<div class="pr-list">
+<ul  class="next"> 
+<li><a class="btn btn-light" id="inv" onclick="openPCard('card-6', 'card-7')">PREVIOUS</a></li>                        
+<li><a class="page active" onclick="openPCard('card-1', 'card-1')" >1</a></li>            
+<li><a class="page" onclick="openPCard('card-2', 'card-1')">2</a></li>            
+<li><a class="page" onclick="openPCard('card-3', 'card-1')">3</a></li>            
+<li><a class="page" onclick="openPCard('card-4', 'card-1')">4</a></li>            
+<li><a class="page" onclick="openPCard('card-5', 'card-1')">5</a></li>            
+<li><a class="page" onclick="openPCard('card-6', 'card-1')">6</a></li>         
+<li><a class="page" onclick="openPCard('card-7', 'card-1')">7</a></li>         
+<li><a class="btn btn-light" id="next" onclick="openPCard('card-2', 'card-1')">NEXT</a></li>               
+</ul> 
+</div>
